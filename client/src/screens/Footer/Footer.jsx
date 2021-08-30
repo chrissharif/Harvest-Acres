8f6bb25d23a0a313006f736cc7eb75fd9558c84e
Christopher Sharif <christophersharif@gmail.com>
2021-08-30 16:04:30 -0400

    footer done

--- client/src/screens/Footer/Footer.jsx
+++ client/src/screens/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import './Footer.css'
 
 export default function Footer() {
@@ -8,6 +9,9 @@ export default function Footer() {
       <div className='footer-name'>Christopher Sharif &copy; 2021</div>
       <a href='https://github.com/chrissharif'>
         <GitHubIcon />
+      </a>
+      <a href='https://www.linkedin.com/in/christopher-sharif-754598211/' className='linked-in'>
+        <LinkedInIcon />
       </a>
     </div>
   )
